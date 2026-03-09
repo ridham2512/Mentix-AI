58a62f5af81efb91f45c4c4aac3a9a7cea2f422c
Ridham <ridhamsharma2514@gmail.com>
2026-03-09 20:19:31 +0530

    Initial commit: Mentix project setup with Next.js and Clerk authentication

---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+import { Outfit } from "next/font/google";
+import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import "./globals.css";
+
+const outfitFont = Outfit({
+  subsets: ["latin"],
+  weight: ['100','200','300','400','500','600','700','800','900'],
+});
+
+export const metadata: Metadata = {
+  title: "Mentix",
+  description: "Mentix is a AI learning platform to connect with other learners in the community and share your learning journey.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${outfitFont.className} antialiased`}>
+
+          <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <Show when="signed-out">
+              <SignInButton />
+              <SignUpButton />
+            </Show>
+
+            <Show when="signed-in">
+              <UserButton />
+            </Show>
+          </header>
+
+          {children}
+
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div>
+    <h1> Hello next.js </h1>
+    </div>
+  );
+}
