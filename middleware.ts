8fabfee6cad8d2ca9dac7bbdf0cd4b1e70577db6
Ridham <ridhamsharma2514@gmail.com>
2026-03-18 21:31:33 +0530

    update middle ware to match all routes except for the ones that are used for static assets and the API routes

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
 export default clerkMiddleware();
-
+//update middle ware to match all routes except for the ones that are used for static assets and the API routes. This is done to ensure that the middleware is applied to all relevant routes while excluding those that do not require authentication or session management. The matcher uses a regular expression to exclude paths that start with "_next" (which is used for Next.js static assets) and any paths that contain a dot (indicating a file extension), as well as paths that start with "api" or "trpc" (which are typically used for API routes).
 export const config = {
   matcher: [
     '/((?!_next|.*\\..*).*)',
