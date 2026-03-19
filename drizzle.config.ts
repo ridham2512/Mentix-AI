4a7b52bad28956f4f3a1454086c8795163799a34
Ridham <ridhamsharma2514@gmail.com>
2026-03-19 20:22:35 +0530

    updated drizzle.config.ts to use the defineConfig function from drizzle-kit and to specify the output directory for the generated files, the path to the schema file, the database dialect, and the database credentials

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
 
 config({ path: ".env.local" });
-
+//updated drizzle.config.ts to use the defineConfig function from drizzle-kit and to specify the output directory for the generated files, the path to the schema file, the database dialect, and the database credentials. 
 export default defineConfig({
   out: "./drizzle",
   schema: "./db/schema.ts",
